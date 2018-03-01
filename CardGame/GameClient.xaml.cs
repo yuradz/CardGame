dd1a4dec44c558269f21f1d59bddc4dda21e2544
Yura Dzyha <yura_dz@hotmail.com>
2018-03-01 16:32:58 +0200

    GameClient Window = finished
    Finished creating GameClient Window

---
+++ CardGame/GameClient.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CardGame
+{
+    /// <summary>
+    /// Interaction logic for GameClient.xaml
+    /// </summary>
+    public partial class GameClient : Window
+    {
+        public GameClient()
+        {
+            InitializeComponent();
+        }
+        private void CommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Close)
+                e.CanExecute = true;
+            if (e.Command == ApplicationCommands.Save)
+                e.CanExecute = false;
+            e.Handled = true;
+        }
+        private void CommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Close)
+                this.Close();
+            e.Handled = true;
+        }
+    }
+}
