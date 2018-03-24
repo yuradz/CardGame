d30bd6f96632b52bde58942b02e6841213388b58
Yura Dzyha <yura_dz@hotmail.com>
2018-03-24 20:53:16 +0200

    Finished creating playboard with cards shown on it
    added 2 classes:
    -CardControl - represents Card Control
    -RankNameConverter - convert Rank into string
    
    added class library:Ch13CardLib

--- CardGame/GameClient.xaml.cs
+++ CardGame/GameClient.xaml.cs
@@ -22,6 +22,24 @@ namespace CardGame
         public GameClient()
         {
             InitializeComponent();
+            var position = new Point(15, 15);
+            for (var i = 0; i < 4; i++)
+            {
+                var suit = (Ch13CardLib.Suit)i;
+                position.Y = 15;
+                for (int rank = 1; rank < 14; rank++)
+                {
+                    position.Y += 30;
+                    var card = new CardControl(new Ch13CardLib.Card((Ch13CardLib.Suit)suit,
+                    (Ch13CardLib.Rank)rank));
+                    card.VerticalAlignment = VerticalAlignment.Top;
+                    card.HorizontalAlignment = HorizontalAlignment.Left;
+                    card.Margin = new Thickness(position.X, position.Y, 0, 0);
+                    contentGrid.Children.Add(card);
+                    Grid.SetRow(card, 2);
+                }
+                position.X += 112;
+            }
         }
         private void CommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
